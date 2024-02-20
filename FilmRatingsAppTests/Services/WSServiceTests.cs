aff635106680b57b0793a9a2d376bcbe589037f9
martmate <martmate@D360-S31>
2024-02-20 17:43:46 +0100

    test

---
+++ FilmRatingsAppTests/Services/WSServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FilmRatingsApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmRatingsApp.Services.Tests
+{
+    [TestClass()]
+    public class WSServiceTests
+    {
+        [TestMethod()]
+        public void WSServiceTest()
+        {
+            //Arrange
+            WSService service = new WSService("https://localhost:7164/api/");
+
+            Assert.IsNotNull(service);
+        }
+
+        [TestMethod()]
+        public void GetUtilisateursTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUtlisateurByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUtlisateurByEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostUtilisateurTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutUtilisateurTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteUtilisateurTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
